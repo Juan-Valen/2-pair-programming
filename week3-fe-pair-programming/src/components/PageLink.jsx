861324bc7782a51b7f11ea8da1aa24b98f41d27a
Su Wai Phyoe <beatrizisme4@gmail.com>
2025-09-03 15:59:07 +0300

    week3 frontend pair programming

--- week3-fe-pair-programming/src/components/PageLink.jsx
+++ week3-fe-pair-programming/src/components/PageLink.jsx
@@ -1,10 +1,10 @@
-    const PageLink = ({ link, itemClass }) => {
-      return (
-        <li key={link.id}>
-          <a href={link.href} className={itemClass}>
-            {link.text}
-          </a>
-        </li>
-      )
-    }
-    export default PageLink
+const PageLink = ({ link, itemClass }) => {
+  return (
+    <li key={link.id}>
+      <a href={link.href} className={itemClass}>
+        {link.text}
+      </a>
+    </li>
+  )
+}
+export default PageLink
