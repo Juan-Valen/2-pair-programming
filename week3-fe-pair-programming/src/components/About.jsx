cb7e32c49ee6351098c4858a09e8b904fd0a2463
Su Wai Phyoe <beatrizisme4@gmail.com>
2025-09-03 15:28:42 +0300

    updated week3 frontend pair programming

--- week3-fe-pair-programming/src/components/About.jsx
+++ week3-fe-pair-programming/src/components/About.jsx
@@ -1,12 +1,9 @@
 import aboutImg from '../assets/images/about.jpeg';
+import Title from './Title';
 const About = () => {
   return (
     <section className="section" id="about">
-      <div className="section-title">
-        <h2>
-          about <span>us</span>
-        </h2>
-      </div>
+      <Title title="About" subTitle="Us" />
       <div className="section-center about-center">
         <div className="about-img">
           <img src={aboutImg} className="about-img" alt="awesome beach" />
