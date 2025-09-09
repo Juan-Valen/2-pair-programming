b118e8cb61fea6da697f0d5b26de9ee058e49afc
john <juan.valenz.101@hotmail.com>
2025-09-09 23:32:49 +0300

    Add registration form

--- week4-fe-pp/src/components/Navbar.jsx
+++ week4-fe-pp/src/components/Navbar.jsx
@@ -3,17 +3,17 @@ import SocialLinks from './SocialLinks';
 import Header from './Header';
 
 const Navbar = () => {
-  return (
-    <div>
-      <nav className="navbar">
-        <div className="nav-center">
-          <Header />
-          <PageLinks parentClass="nav-links" itemClass="nav-link" />
-          <SocialLinks parentClass="nav-icons" itemClass="nav-icon"/>
+    return (
+        <div>
+            <nav className="navbar">
+                <div className="nav-center">
+                    <Header />
+                    <PageLinks parentClass="nav-links" itemClass="nav-link" />
+                    <SocialLinks parentClass="nav-icons" itemClass="nav-icon" />
+                </div>
+            </nav>
         </div>
-      </nav>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
