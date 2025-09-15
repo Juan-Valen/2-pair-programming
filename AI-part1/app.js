82111b0fd969ab74ee136c2ccb98d5b61061ed3f
john <juan.valenz.101@hotmail.com>
2025-09-15 17:19:08 +0300

    Gemini LLM types of queries

---
+++ AI-part1/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+dotenv.config();
+const generateText1  = require("./controllers/textController1");
+const generateText2  = require("./controllers/textController2");
+const generateText3  = require("./controllers/textController3");
+const healthPlan  = require("./controllers/healthPlanController");
+const healthPlanJson  = require("./controllers/healthPlanJsonController");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.post('/api/generate-text1',generateText1 );
+app.post('/api/generate-text2',generateText2 );
+app.post('/api/generate-text3',generateText3 );
+
+app.post('/api/healthPlan', healthPlan );
+app.post('/api/healthPlanJson', healthPlanJson);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+// npm install express dotenv multer @google/genai
