0b1930f4a3611c2efeb8abb87a2e118c01923dc1
john <juan.valenz.101@hotmail.com>
2025-09-17 15:03:24 +0300

    Iteration 3 add a job

--- week5-fepp-starter/frontend/src/pages/AddJobPage.jsx
+++ week5-fepp-starter/frontend/src/pages/AddJobPage.jsx
@@ -2,91 +2,126 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const AddJobPage = () => {
-  const [title, setTitle] = useState("");
-  const [type, setType] = useState("Full-Time");
-  const [location, setLocation] = useState("");
-  const [description, setDescription] = useState("");
-  const [salary, setSalary] = useState(4500);
-  const [companyName, setCompanyName] = useState("");
-  const [contactEmail, setContactEmail] = useState("");
-  const [contactPhone, setContactPhone] = useState("");
+    const [formData, setFormData] = useState({
+        title: "",
+        type: "Full-Time",
+        location: "",
+        description: "",
+        salary: 4500,
+        companyName: "",
+        contactEmail: "",
+        contactPhone: "",
+    });
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setFormData((prev) => ({ ...prev, [id]: value }));
+    };
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const submitForm = (e) => {
-    e.preventDefault();
-    console.log("AddJobPage");
-  };
+    const addJob = async (job) => {
+        try {
+            const res = await fetch('/api/jobs', {
+                method: 'POST',
+                headers: {
+                    "Content-type": "application/json",
+                },
+                body: JSON.stringify(job),
+            });
+            if (res.ok) {
+                navigate('/');
+                console.log("Successful");
+            } else {
+                console.error("Failed to post jobs.");
+            }
+        } catch (err) {
+            console.error("Failed to post jobs: ", err);
+        };
+    }
 
-  return (
-    <div className="create">
-      <h2>Add a New Job</h2>
-      <form onSubmit={submitForm}>
-        <label htmlFor="title">Job title:</label>
-        <input
-          id="title"
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <label htmlFor="type">Job type:</label>
-        <select
-          id="type"
-          value={type}
-          onChange={(e) => setType(e.target.value)}
-        >
-          <option value="" disabled>
-            Select job type
-          </option>
-          <option value="Full-Time">Full-Time</option>
-          <option value="Part-Time">Part-Time</option>
-          <option value="Internship">Internship</option>
-        </select>
-        <label htmlFor="description">Job Description:</label>
-        <textarea
-          id="description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        ></textarea>
-        <label htmlFor="companyName">Company Name:</label>
-        <input
-          id="companyName"
-          type="text"
-          value={companyName}
-          onChange={(e) => setCompanyName(e.target.value)}
-        />
-        <label htmlFor="contactEmail">Contact Email:</label>
-        <input
-          id="contactEmail"
-          type="email"
-          value={contactEmail}
-          onChange={(e) => setContactEmail(e.target.value)}
-        />
-        <label htmlFor="contactPhone">Contact Phone:</label>
-        <input
-          id="contactPhone"
-          type="tel"
-          value={contactPhone}
-          onChange={(e) => setContactPhone(e.target.value)}
-        />
-        <label htmlFor="location">Location:</label>
-        <input
-          id="location"
-          type="text"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-        <label htmlFor="salary">Salary:</label>
-        <input
-          id="salary"
-          type="text"
-          value={salary}
-          onChange={(e) => setSalary(e.target.value)}
-        />
-        <button type="submit">Add Job</button>
-      </form>
-    </div>
-  );
+    const submitForm = (e) => {
+        e.preventDefault();
+        const job = {
+            ...formData,
+            company: {
+                name: formData.companyName,
+                contactEmail: formData.contactEmail,
+                contactPhone: formData.contactPhone,
+            }
+        }
+        addJob(job);
+        console.log("AddJobPage");
+    };
+
+    return (
+        <div className="create">
+            <h2>Add a New Job</h2>
+            <form onSubmit={submitForm}>
+                <label htmlFor="title">Job title:</label>
+                <input
+                    id="title"
+                    type="text"
+                    value={formData.title}
+                    onChange={handleChange}
+                />
+                <label htmlFor="type">Job type:</label>
+                <select
+                    id="type"
+                    value={formData.type}
+                    onChange={handleChange}
+                >
+                    <option value="" disabled>
+                        Select job type
+                    </option>
+                    <option value="Full-Time">Full-Time</option>
+                    <option value="Part-Time">Part-Time</option>
+                    <option value="Internship">Internship</option>
+                </select>
+                <label htmlFor="description">Job Description:</label>
+                <textarea
+                    id="description"
+                    value={formData.description}
+                    onChange={handleChange}
+                ></textarea>
+                <label htmlFor="companyName">Company Name:</label>
+                <input
+                    id="companyName"
+                    type="text"
+                    value={formData.companyName}
+                    onChange={handleChange}
+                />
+                <label htmlFor="contactEmail">Contact Email:</label>
+                <input
+                    id="contactEmail"
+                    type="email"
+                    value={formData.contactEmail}
+                    onChange={handleChange}
+                />
+                <label htmlFor="contactPhone">Contact Phone:</label>
+                <input
+                    id="contactPhone"
+                    type="tel"
+                    value={formData.contactPhone}
+                    onChange={handleChange}
+                />
+                <label htmlFor="location">Location:</label>
+                <input
+                    id="location"
+                    type="text"
+                    value={formData.location}
+                    onChange={handleChange}
+                />
+                <label htmlFor="salary">Salary:</label>
+                <input
+                    id="salary"
+                    type="text"
+                    value={formData.salary}
+                    onChange={handleChange}
+                />
+                <button type="submit">Add Job</button>
+            </form>
+        </div>
+    );
 };
 
 export default AddJobPage;
