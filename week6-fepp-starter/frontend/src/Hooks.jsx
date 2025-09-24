164780873489ed1ce1411e614c00c3d03b188da2
john <juan.valenz.101@hotmail.com>
2025-09-24 14:51:26 +0300

    Week 6 Wednesday Iteration 2 and 3.

---
+++ week6-fepp-starter/frontend/src/Hooks.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const useSignup = (setIsAuthenticated) => {
+    const [form, setForm] = useState({ email: "", password: "" });
+    const navigate = useNavigate();
+    const handleSignup = async () => {
+        try {
+            const res = await fetch("/api/users/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(form)
+            });
+            if (res.ok) {
+                const user = await res.json();
+                localStorage.setItem("user", JSON.stringify(user));
+                console.log("User Signup successfully.");
+                setIsAuthenticated(true);
+                navigate("/");
+            } else {
+                console.error("signup failed!");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return {
+        form,
+        setForm,
+        handleSignup
+    }
+}
+
+export const useLogin = (setIsAuthenticated) => {
+    const [form, setForm] = useState({ email: "", password: "" });
+    const navigate = useNavigate();
+    const handleLogin = async () => {
+        try {
+            const res = await fetch("/api/users/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(form)
+            });
+            if (res.ok) {
+                const user = await res.json();
+                localStorage.setItem("user", JSON.stringify(user));
+                console.log("User Signup successfully.");
+                setIsAuthenticated(true);
+                navigate("/");
+            } else {
+                console.error("signup failed!");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return {
+        form,
+        setForm,
+        handleLogin
+    }
+}
+
+export default useSignup;
--- week6-fepp-starter/frontend/src/pages/LoginComponent.jsx
+++ week6-fepp-starter/frontend/src/pages/LoginComponent.jsx
@@ -1,59 +1,30 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLogin } from "../Hooks";
 
 const LoginComponent = ({ setIsAuthenticated }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+    const { form, setForm, handleLogin } = useLogin(setIsAuthenticated);
 
-  const handleLogin = async () => {
-    try {
-      const response = await fetch("/api/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      });
-
-      if (response.ok) {
-        const user = await response.json();
-        localStorage.setItem("user", JSON.stringify(user));
-        console.log("User logged in successfully!");
-        setIsAuthenticated(true);
-        navigate("/");
-      } else {
-        console.error("Login failed");
-      }
-    } catch (error) {
-      console.error("Error during login:", error);
-    }
-  };
-
-  return (
-    <div className="form-container">
-      <h2>Login</h2>
-      <label>
-        email:
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <br />
-      <button onClick={handleLogin}>Log In</button>
-    </div>
-  );
+    return (
+        <div className="form-container">
+            <h2>Login</h2>
+            <label>
+                email:
+                <input
+                    type="text"
+                    value={form.email}
+                    onChange={(e) => setForm((f) =>{ return {...f, email: e.target.value}})} />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input
+                    type="password"
+                    value={form.password}
+                    onChange={(e) => setForm((f) =>{ return {...f, password: e.target.value}})} />
+            </label>
+            <br />
+            <button onClick={handleLogin}>Log In</button>
+        </div>
+    );
 };
 
 export default LoginComponent;
