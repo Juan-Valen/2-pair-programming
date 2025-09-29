136c3dd1bbfa16481eaa04cad372e30976424c8a
Amir Noori <amir.noori@metropolia.fi>
2025-09-29 15:45:32 +0300

    Part 2 Jobs Iteration 1 done

---
+++ week7-bepp-starter/backend-auth/tests/data.js
@@ -0,0 +1,24 @@
+const jobs = [
+    {
+        title: "Helsinki in 5 Days Tour",
+        type: "city",
+        description: "Helsinki city",
+        company: {
+            name: "Heka",
+            contactEmail: "info@heka.fi",
+            contactPhone: "+358521648845",
+        },
+    },
+    {
+        title: "Vanta in 5 Days Tour",
+        type: "city",
+        description: "Vanta city",
+        company: {
+            name: "Veka",
+            contactEmail: "info@veka.fi",
+            contactPhone: "+358521648854",
+        },
+    },
+];
+
+module.exports = jobs;
