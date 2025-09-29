980f2bc1f0403edd82bce5adef5384d98da8c948
Amir Noori <amir.noori@metropolia.fi>
2025-09-29 12:26:08 +0300

    Part 2 User Authentication

--- week7-fepp-starter/frontend/src/components/Navbar.jsx
+++ week7-fepp-starter/frontend/src/components/Navbar.jsx
@@ -5,6 +5,8 @@ const Navbar = () => {
       <div className="links">
         <a href="/">Home</a>
         <a href="/add-job">Add Job</a>
+        <a href="/signup">Sign up</a>
+        <a href="/login">Login</a>
       </div>
     </nav>
   );
